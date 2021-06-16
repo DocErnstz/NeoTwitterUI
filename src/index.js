6e46a41e73571e6c524525c0a1a64bf82a3b2d8b
DocErnstz <ernesto800@outlook.com>
2021-06-16 11:31:06 -0300

    convert react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return <h1>React App</h1>
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));
